fbd6959d79141a924d16cec873824b30b20fabbd
john rudisill <jrudisill@gmail.com>
2018-08-20 13:32:37 -0400

    added try logic to db creation so we don't fail if the db already exists

--- flask_template/db_setup.py
+++ flask_template/db_setup.py
@@ -1,5 +1,10 @@
 import db_interface as dbi
 
-dbi.create_db()
+try:
+    dbi.create_db()
+except Exception as e:
+    print(e)
+
+
 dbi.ex_statement(dbi.get_sql('create_users.sql'))
 dbi.add_user('John')
